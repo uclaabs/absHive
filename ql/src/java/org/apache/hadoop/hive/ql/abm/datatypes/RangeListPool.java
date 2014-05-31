72402cdd508d687e8383b9b47268491b74aa0e69
Kai Zeng <kzeng@cs.ucla.edu>
2014-05-31 16:28:57 -0700

    util for reusing rangelists

---
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/datatypes/RangeListPool.java
@@ -0,0 +1,20 @@
+package org.apache.hadoop.hive.ql.abm.datatypes;
+
+import java.util.Stack;
+
+public class RangeListPool extends Stack<RangeList> {
+
+  private static final long serialVersionUID = 1L;
+
+  public RangeList borrow() {
+    if (!isEmpty()) {
+      return pop();
+    }
+    return new RangeList();
+  }
+
+  public void yield(RangeList o) {
+    this.push(o);
+  }
+
+}
