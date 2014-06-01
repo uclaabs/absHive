f8aec4ecd703c1ca94b4df58355bb039ed53d752
Kai Zeng <kzeng@cs.ucla.edu>
2014-05-31 22:53:52 -0700

    nothing big

--- ql/src/java/org/apache/hadoop/hive/ql/abm/udaf/CondMergeEvaluator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/udaf/CondMergeEvaluator.java
@@ -27,27 +27,31 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
   private DoubleObjectInspector rangeValueOI;
   private StructField keyField;
   private StructField rangeField;
-  
-  private static ListObjectInspector longMatrixOI = ObjectInspectorFactory.getStandardListObjectInspector(CondList.longListOI);
-  private static ListObjectInspector doubleMatrixOI = ObjectInspectorFactory.getStandardListObjectInspector(CondList.doubleMatrixOI);
+
+  private static ListObjectInspector longMatrixOI = ObjectInspectorFactory
+      .getStandardListObjectInspector(CondList.longListOI);
+  private static ListObjectInspector doubleMatrixOI = ObjectInspectorFactory
+      .getStandardListObjectInspector(CondList.doubleMatrixOI);
   private static List<String> columnName = Arrays.asList("Keys", "Ranges");
-  private static List<ObjectInspector> ObjectInspectorType = Arrays.asList((ObjectInspector)longMatrixOI, (ObjectInspector) doubleMatrixOI);
-  private static StructObjectInspector partialOI = ObjectInspectorFactory.getStandardStructObjectInspector(columnName, ObjectInspectorType);
-  
-  private KeyWrapper key = new KeyWrapper();
+  private static List<ObjectInspector> ObjectInspectorType = Arrays.asList(
+      (ObjectInspector) longMatrixOI, (ObjectInspector) doubleMatrixOI);
+  private static StructObjectInspector partialOI = ObjectInspectorFactory
+      .getStandardStructObjectInspector(columnName, ObjectInspectorType);
+
+  private final KeyWrapper key = new KeyWrapper();
 
   @Override
   public ObjectInspector init(Mode m, ObjectInspector[] parameters) throws HiveException {
     super.init(m, parameters);
 
-    if(parameters[0].getCategory() != ObjectInspector.Category.STRUCT) {
+    if (parameters[0].getCategory() != ObjectInspector.Category.STRUCT) {
       throw new UDFArgumentLengthException("CondMerge: Incorrect Input Type");
     }
-    inputOI = (StructObjectInspector)parameters[0];
+    inputOI = (StructObjectInspector) parameters[0];
     keyField = inputOI.getStructFieldRef("Keys");
     rangeField = inputOI.getStructFieldRef("Ranges");
-    
-    if(m == Mode.PARTIAL1 || m == Mode.FINAL) {
+
+    if (m == Mode.PARTIAL1 || m == Mode.FINAL) {
       keyOI = (ListObjectInspector) keyField.getFieldObjectInspector();
       rangeOI = (ListObjectInspector) rangeField.getFieldObjectInspector();
     } else {
@@ -56,19 +60,19 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
       keyOI = (ListObjectInspector) keyGroupOI.getListElementObjectInspector();
       rangeOI = (ListObjectInspector) rangeGroupOI.getListElementObjectInspector();
     }
-    
+
     rangeListOI = (ListObjectInspector) rangeOI.getListElementObjectInspector();
     keyValueOI = (LongObjectInspector) keyOI.getListElementObjectInspector();
     rangeValueOI = (DoubleObjectInspector) rangeListOI.getListElementObjectInspector();
 
-    
-    if(m == Mode.PARTIAL1 || m == Mode.PARTIAL2) {
+
+    if (m == Mode.PARTIAL1 || m == Mode.PARTIAL2) {
       // partialTerminate() will be called
       return partialOI;
     } else {
       return CondList.condListOI;
     }
-    
+
   }
 
   private static class MyAggregationBuffer implements AggregationBuffer {
@@ -78,12 +82,11 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
     Object[] partialRet = new Object[2];
     List<Object> keyRet = new ArrayList<Object>();
     List<Object> rangeRet = new ArrayList<Object>();
-    
-    
-    DoubleObjectInspector rangeValueOI;
+
     ListObjectInspector rangeListOI;
-    
-    public MyAggregationBuffer(ObjectInspector rangeValOI, ListObjectInspector rangeList) {
+    DoubleObjectInspector rangeValueOI;
+
+    public MyAggregationBuffer(ListObjectInspector rangeList, ObjectInspector rangeValOI) {
       this.rangeValueOI = (DoubleObjectInspector) rangeValOI;
       this.rangeListOI = rangeList;
     }
@@ -102,7 +105,7 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
       if (ranges != null) {
         for (int i = 0; i < length; ++i) {
           Object listObj = loi.getListElement(o, i);
-          for(int j = 0; j < rangeListOI.getListLength(listObj); j ++ ) {
+          for (int j = 0; j < rangeListOI.getListLength(listObj); j++) {
             ranges.get(i).add(rangeValueOI.get(rangeListOI.getListElement(listObj, j)));
           }
         }
@@ -112,7 +115,7 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
         for (int i = 0; i < length; ++i) {
           RangeList rl = new RangeList();
           Object listObj = loi.getListElement(o, i);
-          for(int j = 0; j < rangeListOI.getListLength(listObj); j ++ ) {
+          for (int j = 0; j < rangeListOI.getListLength(listObj); j++) {
             rl.add(rangeValueOI.get(rangeListOI.getListElement(listObj, j)));
           }
           ranges.add(rl);
@@ -125,12 +128,12 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
 
       return index;
     }
-    
+
     public Object getPartialObj() {
       keyRet.clear();
       rangeRet.clear();
       //
-      for(Map.Entry<KeyWrapper, List<RangeList>> entry:groups.entrySet()) {
+      for (Map.Entry<KeyWrapper, List<RangeList>> entry : groups.entrySet()) {
         keyRet.add(entry.getKey());
         rangeRet.add(entry.getValue());
       }
@@ -147,34 +150,29 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
 
   @Override
   public AggregationBuffer getNewAggregationBuffer() throws HiveException {
-    return new MyAggregationBuffer(this.rangeValueOI, this.rangeListOI);
+    return new MyAggregationBuffer(this.rangeListOI, this.rangeValueOI);
   }
-  
+
   protected boolean isBase(Object rangeObj, int i) {
-    
     double lower = this.rangeValueOI.get(this.rangeListOI.getListElement(rangeObj, 0));
     double upper = this.rangeValueOI.get(this.rangeListOI.getListElement(rangeObj, 1));
-    if(lower == Double.NEGATIVE_INFINITY && upper == Double.POSITIVE_INFINITY) {
+    if (lower == Double.NEGATIVE_INFINITY && upper == Double.POSITIVE_INFINITY) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
 
   @Override
   public void iterate(AggregationBuffer agg, Object[] parameters) throws HiveException {
-    
     if (parameters[0] != null) {
-      
-      
-      Object keyObj = this.inputOI.getStructFieldData(parameters[0],this.keyField);
+      Object keyObj = this.inputOI.getStructFieldData(parameters[0], this.keyField);
       Object rangeObj = this.inputOI.getStructFieldData(parameters[0], this.rangeField);
 
       boolean isBase = true;
       for (int i = 0; i < this.rangeOI.getListLength(rangeObj); i++) {
         Object rangeListObj = this.rangeOI.getListElement(rangeObj, i);
-        if(isBase(rangeListObj, i)) {
+        if (isBase(rangeListObj, i)) {
           isBase = false;
           break;
         }
@@ -186,7 +184,7 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
 
       MyAggregationBuffer myagg = (MyAggregationBuffer) agg;
       key.parseKey(keyObj, keyOI, keyValueOI);
-      
+
       // Put the tuples in input Condition List to different groups
       int inst = myagg.addRanges(key, rangeObj, this.rangeOI);
       // Set the instruction here
@@ -204,58 +202,56 @@ public class CondMergeEvaluator extends GenericUDAFEvaluatorWithInstruction {
 
   @Override
   public Object terminatePartial(AggregationBuffer agg) throws HiveException {
-    
+
     MyAggregationBuffer myagg = (MyAggregationBuffer) agg;
     return myagg.getPartialObj();
 
-//    Object[] keys = new Object[myagg.groups.size()];
-//    Object[] values = new Object[myagg.groups.size()];
-//
-//    int i = 0;
-//    for (Map.Entry<KeyWrapper, List<RangeList>> entry : myagg.groups.entrySet()) {
-//      keys[i] = entry.getKey().toArray();
-//      List<RangeList> rangeGroup = entry.getValue();
-//
-//      // TODO:
-//      Object[] rangeMatrix = new Object[rangeGroup.size()];
-//      for (int j = 0; j < rangeGroup.size(); j++) {
-//        Object[] rangeArray = new Object[rangeGroup.get(j).size()];
-//        List<ConditionRange> rangeList = rangeGroup.get(j);
-//        for (int k = 0; k < rangeList.size(); k++) {
-//          rangeArray[k] = rangeList.get(k).toArray();
-//        }
-//        rangeMatrix[j] = rangeArray;
-//      }
-//      values[i] = rangeMatrix;
-//      ++i;
-//    }
-//
-//    return new Object[] {keys, values};
-    
+    // Object[] keys = new Object[myagg.groups.size()];
+    // Object[] values = new Object[myagg.groups.size()];
+    //
+    // int i = 0;
+    // for (Map.Entry<KeyWrapper, List<RangeList>> entry : myagg.groups.entrySet()) {
+    // keys[i] = entry.getKey().toArray();
+    // List<RangeList> rangeGroup = entry.getValue();
+    //
+    // // TODO:
+    // Object[] rangeMatrix = new Object[rangeGroup.size()];
+    // for (int j = 0; j < rangeGroup.size(); j++) {
+    // Object[] rangeArray = new Object[rangeGroup.get(j).size()];
+    // List<ConditionRange> rangeList = rangeGroup.get(j);
+    // for (int k = 0; k < rangeList.size(); k++) {
+    // rangeArray[k] = rangeList.get(k).toArray();
+    // }
+    // rangeMatrix[j] = rangeArray;
+    // }
+    // values[i] = rangeMatrix;
+    // ++i;
+    // }
+    //
+    // return new Object[] {keys, values};
+
   }
 
   @Override
   public void merge(AggregationBuffer agg, Object partialRes) throws HiveException {
-    
     if (partialRes == null) {
       return;
     }
+
     MyAggregationBuffer myagg = (MyAggregationBuffer) agg;
-    Object keyGroupObj = this.inputOI.getStructFieldData(partialRes,this.keyField);
+    Object keyGroupObj = this.inputOI.getStructFieldData(partialRes, this.keyField);
     Object rangeGroupObj = this.inputOI.getStructFieldData(partialRes, this.rangeField);
-    
-    for(int i = 0; i < this.keyGroupOI.getListLength(keyGroupObj); i ++) {
-      
-      
+
+    for (int i = 0; i < this.keyGroupOI.getListLength(keyGroupObj); i++) {
       Object keyObj = this.keyGroupOI.getListElement(keyGroupObj, i);
       Object rangeObj = this.rangeGroupOI.getListElement(rangeGroupObj, i);
-      
+
       key.parseKey(keyObj, keyOI, keyValueOI);
-      
+
       int inst = myagg.addRanges(key, rangeObj, this.rangeOI);
       ins.addGroupInstruction(inst);
     }
-    
+
   }
 
   @Override
