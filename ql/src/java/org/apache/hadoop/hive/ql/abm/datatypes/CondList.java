e7f1fcaa2bc800301018867fdbad7d3b786d7f49
Kai Zeng <kzeng@cs.ucla.edu>
2014-06-01 22:12:06 -0700

    clean CondList

--- ql/src/java/org/apache/hadoop/hive/ql/abm/datatypes/CondList.java
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/datatypes/CondList.java
@@ -12,64 +12,52 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 
 public class CondList {
 
-  KeyWrapper keyList;
-  List<RangeList> rangeMatrix;
-  Object[] ret;
-  List<Object> rangeRet;
+  private final KeyWrapper keyList = new KeyWrapper();
+  private final List<RangeList> rangeMatrix = new ArrayList<RangeList>();
+  private final Object[] ret = {keyList, rangeMatrix};
 
   public static ListObjectInspector longListOI = ObjectInspectorFactory
       .getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaLongObjectInspector);
   public static ListObjectInspector doubleMatrixOI = ObjectInspectorFactory
       .getStandardListObjectInspector(
       ObjectInspectorFactory
-          .getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector)); // TODO
+          .getStandardListObjectInspector(PrimitiveObjectInspectorFactory.javaDoubleObjectInspector));
   public static List<String> columnName = Arrays.asList("Keys", "Ranges");
   public static List<ObjectInspector> objectInspectorType = Arrays.asList(
       (ObjectInspector) longListOI, (ObjectInspector) doubleMatrixOI);
   public static StructObjectInspector condListOI = ObjectInspectorFactory
       .getStandardStructObjectInspector(columnName, objectInspectorType);
 
-  public CondList() {
-    keyList = new KeyWrapper();
-    rangeMatrix = new ArrayList<RangeList>();
-    ret = new Object[2];
-    rangeRet = new ArrayList<Object>();
-  }
-
-  public void update(long id, double value) {
+  public void update(int id, double value) {
     keyList.set(0, id);
     rangeMatrix.get(0).set(0, value);
   }
 
-  public void update(long id1, long id2, double value) {
+  public void update(int id1, int id2, double value) {
     keyList.set(0, id1);
     keyList.set(1, id2);
     rangeMatrix.get(0).set(0, value);
   }
 
-  public void addKey(long key) {
-    this.keyList.add(key);
+  public void addKey(int key) {
+    keyList.add(key);
   }
 
   public void addRangeValue(double value) {
     RangeList newlist = new RangeList();
     newlist.add(value);
-    this.rangeMatrix.add(newlist);
+    rangeMatrix.add(newlist);
   }
 
   public void addKeys(KeyWrapper newKeys) {
-    this.keyList.addAll(newKeys);
+    keyList.addAll(newKeys);
   }
 
   public void addRanges(RangeList newRanges) {
-    this.rangeMatrix.add(newRanges);
+    rangeMatrix.add(newRanges);
   }
 
   public void addRange(double[] rangeArray) {
-//    for (int i = 0; i < rangeArray.length / 2; i++) {
-//      this.rangeMatrix.get(i).add(rangeArray[2 * i]);
-//      this.rangeMatrix.get(i).add(rangeArray[2 * i + 1]);
-//    }
     for(int i = 0; i < rangeArray.length; i ++) {
       this.rangeMatrix.get(i).add(rangeArray[i]);
     }
@@ -81,8 +69,6 @@ public class CondList {
   }
 
   public Object toArray() {
-    ret[0] = keyList;
-    ret[1] = rangeMatrix;
     return ret;
   }
 
