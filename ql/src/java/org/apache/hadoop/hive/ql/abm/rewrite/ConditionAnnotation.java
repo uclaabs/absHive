532588fc76be7fc74763cfe3e37a8daf017f2b7c
Kai Zeng <Kai@kai.(none)>
2014-06-04 00:18:17 -0700

    apply the new implementation for Merge; set final select to read cached input/output

--- ql/src/java/org/apache/hadoop/hive/ql/abm/rewrite/ConditionAnnotation.java
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/rewrite/ConditionAnnotation.java
@@ -14,8 +14,6 @@ import org.apache.hadoop.hive.ql.abm.lib.TopologicalSort;
 import org.apache.hadoop.hive.ql.exec.GroupByOperator;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.SelectOperator;
-import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
-import org.apache.hadoop.hive.ql.plan.GroupByDesc;
 import org.apache.hadoop.hive.ql.plan.OperatorDesc;
 
 public class ConditionAnnotation {
@@ -32,9 +30,9 @@ public class ConditionAnnotation {
       new HashMap<GroupByOperator, SelectOperator>();
 
   private List<List<GroupByOperator>> sorted = null;
+  private List<GroupByOperator> sortedContinuous = null;
+  private List<GroupByOperator> sortedDiscrete = null;
   private Dictionary<GroupByOperator> gbyDict = null;
-  private Dictionary<AggregateInfo> aggrDict = null;
-
   // <-- Used by TraceProcCtx
 
   public void groupByAt(GroupByOperator gby) {
@@ -85,161 +83,81 @@ public class ConditionAnnotation {
     outputs.put(gby, output);
   }
 
-  public int getAggregateId(AggregateInfo ai) {
-    if (aggrDict == null) {
+  public int[] getAggregateId(AggregateInfo ai) {
+    if (sorted == null) {
       setupIds();
     }
-    return aggrDict.get(ai);
+    return new int[] {
+        gbyDict.get(ai.getGroupByOperator()),
+        ai.getIndex() != -1 ? ai.getIndex() : aggregates.get(ai.getGroupByOperator()).size()
+    };
   }
 
   private void setupIds() {
     Map<GroupByOperator, Set<GroupByOperator>> map = getDependencyGraph();
     sorted = TopologicalSort.getOrderByLevel(map);
 
-    // Assign ids to GroupByOperators
-    List<GroupByOperator> gbys = new ArrayList<GroupByOperator>();
-    for (List<GroupByOperator> level : sorted) {
-      gbys.addAll(level);
-    }
-    gbyDict = new Dictionary<GroupByOperator>(gbys);
-
-    // Assign ids to AggregateInfos
-    List<AggregateInfo> aggrs = new ArrayList<AggregateInfo>();
+    Set<GroupByOperator> continuous = getAllContinousGbys();
+    sortedContinuous = new ArrayList<GroupByOperator>();
+    sortedDiscrete = new ArrayList<GroupByOperator>();
     for (List<GroupByOperator> level : sorted) {
       for (GroupByOperator gby : level) {
-        aggrs.addAll(aggregates.get(gby));
+        if (continuous.contains(gby)) {
+          sortedContinuous.add(gby);
+        } else {
+          sortedDiscrete.add(gby);
+        }
       }
     }
-    aggrDict = new Dictionary<AggregateInfo>(aggrs);
+
+    // Assign ids to GroupByOperators
+    List<GroupByOperator> gbys = new ArrayList<GroupByOperator>();
+    gbys.addAll(sortedContinuous);
+    gbys.addAll(sortedDiscrete);
+    gbyDict = new Dictionary<GroupByOperator>(gbys);
   }
 
   public void setupMCSim(SelectOperator select) {
-    ArrayList<GroupByOperator> cGbys = new ArrayList<GroupByOperator>(getAllContinousGbys());
-    ArrayList<GroupByOperator> dGbys = new ArrayList<GroupByOperator>(getAllDiscreteGbys());
-
+    // Rearrange the connections
     ArrayList<Operator<? extends OperatorDesc>> inputOps = new ArrayList<Operator<? extends OperatorDesc>>();
-    ArrayList<Operator<? extends OperatorDesc>> outputCOps = new ArrayList<Operator<? extends OperatorDesc>>();
-    ArrayList<Integer> cTags = new ArrayList<Integer>();
-    for (GroupByOperator gby : cGbys) {
+    ArrayList<Operator<? extends OperatorDesc>> outputContinuousOps = new ArrayList<Operator<? extends OperatorDesc>>();
+    for (GroupByOperator gby : sortedContinuous) {
       inputOps.add(inputs.get(gby));
-      outputCOps.add(outputs.get(gby));
-      cTags.add(gbyDict.get(gby));
+      outputContinuousOps.add(outputs.get(gby));
     }
-
-    ArrayList<Operator<? extends OperatorDesc>> outputDOps = new ArrayList<Operator<? extends OperatorDesc>>();
-    ArrayList<Integer> dTags = new ArrayList<Integer>();
-    for (GroupByOperator gby : dGbys) {
-      outputDOps.add(outputs.get(gby));
-      dTags.add(gbyDict.get(gby));
+    ArrayList<Operator<? extends OperatorDesc>> outputDiscreteOps = new ArrayList<Operator<? extends OperatorDesc>>();
+    for (GroupByOperator gby : sortedDiscrete) {
+      outputDiscreteOps.add(outputs.get(gby));
     }
 
     List<Operator<? extends OperatorDesc>> parents = select.getParentOperators();
     parents.addAll(inputOps);
-    parents.addAll(outputCOps);
-    parents.addAll(outputDOps);
-    select.setParentOperators(parents);
+    parents.addAll(outputContinuousOps);
+    parents.addAll(outputDiscreteOps);
     for (Operator<? extends OperatorDesc> p : inputOps) {
       p.getChildOperators().add(select);
     }
-    for (Operator<? extends OperatorDesc> p : outputCOps) {
+    for (Operator<? extends OperatorDesc> p : outputContinuousOps) {
       p.getChildOperators().add(select);
     }
-    for (Operator<? extends OperatorDesc> p : outputDOps) {
+    for (Operator<? extends OperatorDesc> p : outputDiscreteOps) {
       p.getChildOperators().add(select);
     }
 
-    // Continuous input (no input cached for discrete GBYs)
-    ArrayList<ArrayList<ExprNodeDesc>> inKeys = new ArrayList<ArrayList<ExprNodeDesc>>();
-    ArrayList<ArrayList<ExprNodeDesc>> inVals = new ArrayList<ArrayList<ExprNodeDesc>>();
-    ArrayList<ExprNodeDesc> inTids = new ArrayList<ExprNodeDesc>();
-    for (GroupByOperator gby : cGbys) {
-      ArrayList<ExprNodeDesc> keys = new ArrayList<ExprNodeDesc>();
-      ArrayList<ExprNodeDesc> vals = new ArrayList<ExprNodeDesc>();
-      ExprNodeDesc tid = null;
-      GroupByDesc desc = gby.getConf();
-      SelectOperator input = inputs.get(gby);
-
-      int i = 0;
-      for (; i < desc.getKeys().size(); ++i) {
-        keys.add(Utils.generateColumnDescs(input, i).get(0));
-      }
-      for (AggregateInfo ai : aggregates.get(gby)) {
-        if (!ai.getUdafType().equals(UdafType.COUNT)) {
-          vals.add(Utils.generateColumnDescs(input, i++).get(0));
-        }
-      }
-      tid = Utils.generateColumnDescs(input, i).get(0);
-
-      inKeys.add(keys);
-      inVals.add(vals);
-      inTids.add(tid);
+    // Continuous input (no input cached for discrete GBYs): keys, vals, tid
+    // Continuous output: keys, aggregates, lineage, condition, gby-id
+    ArrayList<Integer> numKeysContinuous = new ArrayList<Integer>();
+    for (GroupByOperator gby : sortedContinuous) {
+      numKeysContinuous.add(gby.getConf().getKeys().size());
     }
 
-    // Continuous output
-    ArrayList<ArrayList<ExprNodeDesc>> outCKeys = new ArrayList<ArrayList<ExprNodeDesc>>();
-    ArrayList<ArrayList<ExprNodeDesc>> outCAggrs = new ArrayList<ArrayList<ExprNodeDesc>>();
-    ArrayList<ExprNodeDesc> outCLins = new ArrayList<ExprNodeDesc>();
-    ArrayList<ExprNodeDesc> outCConds = new ArrayList<ExprNodeDesc>();
-    ArrayList<ExprNodeDesc> outCGbyIds = new ArrayList<ExprNodeDesc>();
-    ArrayList<ArrayList<UdafType>> outCTypes = new ArrayList<ArrayList<UdafType>>();
-    for (GroupByOperator gby : cGbys) {
-      ArrayList<ExprNodeDesc> keys = new ArrayList<ExprNodeDesc>();
-      ArrayList<ExprNodeDesc> aggrs = new ArrayList<ExprNodeDesc>();
-      ExprNodeDesc lin = null;
-      ExprNodeDesc cond = null;
-      ExprNodeDesc gbyId = null;
-      ArrayList<UdafType> types = new ArrayList<UdafType>();
-      GroupByDesc desc = gby.getConf();
-      SelectOperator output = outputs.get(gby);
-
-      int i = 0;
-      for (; i < desc.getKeys().size(); ++i) {
-        keys.add(Utils.generateColumnDescs(output, i).get(0));
-      }
-      for (int j = 0, sz = aggregates.get(gby).size(); j < sz; ++j) {
-        aggrs.add(Utils.generateColumnDescs(output, i++).get(0));
-      }
-      lin = Utils.generateColumnDescs(output, i++).get(0);
-      cond = Utils.generateColumnDescs(output, i++).get(0);
-      gbyId = Utils.generateColumnDescs(output, i).get(0);
-      for (AggregateInfo ai : aggregates.get(gby)) {
-        types.add(ai.getUdafType());
-      }
-
-      outCKeys.add(keys);
-      outCAggrs.add(aggrs);
-      outCLins.add(lin);
-      outCConds.add(cond);
-      outCGbyIds.add(gbyId);
-      outCTypes.add(types);
+    // Discrete output: aggregates, condition, gby-id
+    ArrayList<Integer> numKeysDiscrete = new ArrayList<Integer>();
+    for (GroupByOperator gby : sortedDiscrete) {
+      numKeysDiscrete.add(gby.getConf().getKeys().size());
     }
 
-    // Discrete output
-    ArrayList<ArrayList<ExprNodeDesc>> outDAggrs = new ArrayList<ArrayList<ExprNodeDesc>>();
-    ArrayList<ExprNodeDesc> outDConds = new ArrayList<ExprNodeDesc>();
-    ArrayList<ExprNodeDesc> outDGbyIds = new ArrayList<ExprNodeDesc>();
-    for (GroupByOperator gby : dGbys) {
-      ArrayList<ExprNodeDesc> aggrs = new ArrayList<ExprNodeDesc>();
-      ExprNodeDesc cond = null;
-      ExprNodeDesc gbyId = null;
-      GroupByDesc desc = gby.getConf();
-      SelectOperator output = outputs.get(gby);
-
-      int i = desc.getKeys().size();
-      for (int j = 0, sz = aggregates.get(gby).size(); j < sz; ++j) {
-        aggrs.add(Utils.generateColumnDescs(output, i++).get(0));
-      }
-      cond = Utils.generateColumnDescs(output, i++).get(0);
-      gbyId = Utils.generateColumnDescs(output, i).get(0);
-
-      outDAggrs.add(aggrs);
-      outDConds.add(cond);
-      outDGbyIds.add(gbyId);
-    }
-
-    select.getConf().setMCSim(cTags, dTags, inKeys, inVals, inTids,
-        outCKeys, outCAggrs, outCLins, outCConds, outCGbyIds, outCTypes, outDAggrs, outDConds,
-        outDGbyIds);
+    select.getConf().setMCSim(numKeysContinuous, numKeysDiscrete);
 
     // TODO: GBYs' dependency structure
     // TODO: Detailed structure (of each predicate) of every condition column
@@ -262,12 +180,6 @@ public class ConditionAnnotation {
 
   private Set<GroupByOperator> getAllContinousGbys() {
     return inputs.keySet();
-  }
-
-  private Set<GroupByOperator> getAllDiscreteGbys() {
-    Set<GroupByOperator> ret = new HashSet<GroupByOperator>(outputs.keySet());
-    ret.removeAll(getAllContinousGbys());
-    return ret;
   }
 
   public List<Boolean> getCondFlags(GroupByOperator gby) {
