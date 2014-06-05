9aeb2e35493f181f1f47650f10f308cf9712b9d2
Kai Zeng <kzeng@cs.ucla.edu>
2014-06-04 21:59:22 -0700

    correct samples in simulation back to doublearraylist

--- ql/src/java/org/apache/hadoop/hive/ql/abm/udf/simulation/GenericUDFWithSimulation.java
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/udf/simulation/GenericUDFWithSimulation.java
@@ -1,13 +1,15 @@
 package org.apache.hadoop.hive.ql.abm.udf.simulation;
 
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 
 public abstract class GenericUDFWithSimulation extends GenericUDF {
 
   protected int numSimulation;
-  protected double[] samples; // TODO: should use DoubleArrayList, hack for now
+  protected DoubleArrayList samples;
 
-  public void setSamples(double[] samples) {
+  public void setSamples(DoubleArrayList samples) {
     this.samples = samples;
   }
 
