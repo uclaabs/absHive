38ab4d2b98732ff8072d7af887ff52f57f859b81
Kai Zeng <kzeng@cs.ucla.edu>
2014-06-04 17:37:14 -0700

    hack simulation samples

--- ql/src/java/org/apache/hadoop/hive/ql/abm/udf/simulation/GenericUDFWithSimulation.java
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/udf/simulation/GenericUDFWithSimulation.java
@@ -1,15 +1,13 @@
 package org.apache.hadoop.hive.ql.abm.udf.simulation;
 
-import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
-
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 
 public abstract class GenericUDFWithSimulation extends GenericUDF {
 
   protected int numSimulation;
-  protected DoubleArrayList samples;
+  protected double[] samples; // TODO: should use DoubleArrayList, hack for now
 
-  public void setSamples(DoubleArrayList samples) {
+  public void setSamples(double[] samples) {
     this.samples = samples;
   }
 
