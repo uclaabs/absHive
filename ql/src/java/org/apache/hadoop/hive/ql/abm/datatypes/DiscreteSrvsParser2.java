c3e057dcd43ca56446492b6d5b4e590ce0464ded
nlaptev <sgao@Gao-cs-ucla.(none)>
2014-06-14 22:09:16 -0700

    add SrvParser2 to return double[]

---
+++ ql/src/java/org/apache/hadoop/hive/ql/abm/datatypes/DiscreteSrvsParser2.java
@@ -0,0 +1,31 @@
+package org.apache.hadoop.hive.ql.abm.datatypes;
+
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+
+
+public class DiscreteSrvsParser2 extends SrvParser {
+
+  public DiscreteSrvsParser2(ObjectInspector oi, int from, int to) {
+    super(oi, from, to);
+  }
+
+  public double[] parse(Object o) {
+    int fieldLen = fields.length;
+    for(int i = 0; i < fieldLen; i ++) {
+      objs[i] = oi.getStructFieldData(o, fields[i]);
+    }
+    
+    int srvLen = listOIs[0].getListLength(objs[0]) - 2;
+    double[] srvs = new double[srvLen * fieldLen];
+    // every time read one double value from each list and save it to srvs
+    int index = 0;
+    for(int i = 2; i < srvLen; i ++) {
+     for(int j = 0; j < fieldLen; j ++) {
+       srvs[index] = valueOIs[j].get(listOIs[j].getListElement(objs[j], index));
+       index ++;
+     }
+    }
+    return srvs;
+  }
+
+}
